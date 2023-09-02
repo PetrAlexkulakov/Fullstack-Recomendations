a8655fce27472388db49118cecaa61396e63453a
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-09-02 18:21:39 +0400

    feat: improve design

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -4,16 +4,16 @@ import { dataToString } from "../../shared/dataToString"
 const Card = ({ post }: { post: Post }) => {
   return (
     <>
-        <img className="card-img-bottom" src={post.imageURL} alt="..." />
-        <div className="card-body d-flex flex-column justify-content-end align-items-center">
-            <div className="badge bg-secondary text-decoration-none link-light w-25">{post.group}</div>
-            <div className="small text-muted">{dataToString(post.createdAt)}</div>
-            <h2 className="card-title h4">{post.title}</h2>
-            <p className="card-text">{post.smallText}</p>
-            <a className="btn btn-primary" href={`/post/${post.id}`}>Read more →</a>
-        </div>
+      <div className="card-body d-flex flex-column justify-content-end align-items-center">
+        <img className="w-100 pb-2" src={post.imageURL} alt="..." />
+        <div className="badge bg-secondary text-decoration-none link-light w-25">{post.group}</div>
+        <div className="small text-muted">{dataToString(post.createdAt)}</div>
+        <h2 className="card-title h4">{post.title}</h2>
+        <p className="card-text">{post.smallText}</p>
+        <a className="btn btn-primary" href={`/post/${post.id}`}>Read more →</a>
+      </div>
     </>
   )
 }
-
+//className="card-img-bottom"
 export default Card
