4db068b79e865c7f74c8e1e43196ea8e8256244b
PetrAlexkulakov <petr.alex.kulakov@gmail.com>
2023-09-02 19:27:41 +0400

    fix: fix search in params

--- client/src/components/PageWrapper/Search/index.tsx
+++ client/src/components/PageWrapper/Search/index.tsx
@@ -3,22 +3,29 @@ import queryString from 'query-string';
 import { useState } from "react";
 
 const Search = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const navigator = useNavigate();
-
   const location = useLocation();
   const queryParams = queryString.parse(location.search)
   const activeSearch = queryParams.search;
+  const [searchTerm, setSearchTerm] = useState(activeSearch as string || "");
+  const navigator = useNavigate();
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
   const handleSearch = () => {
-    const newQueryParams = {
-      ...queryParams,
-      search: searchTerm
-    };
+    let newQueryParams;
+    if (searchTerm) {
+      newQueryParams = {
+        ...queryParams,
+        search: searchTerm
+      };
+    } else {
+      newQueryParams = {
+        ...queryParams,
+        search: undefined
+      };
+    }
     navigator(`?` + queryString.stringify(newQueryParams));
   };
 
@@ -30,7 +37,7 @@ const Search = () => {
         placeholder="Enter search term..."
         aria-label="Enter search term..."
         aria-describedby="button-search"
-        value={searchTerm || activeSearch as string || ""}
+        value={searchTerm}
         onChange={handleInputChange}
         onKeyDown={(e) => {
             if (e.key === "Enter") {
